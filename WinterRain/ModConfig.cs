858fc060a8f12f4b3fc7d5a4554c46dc537908e9
idermailer <idermailer@gmail.com>
2022-09-14 04:15:33 +0900

    초기 커밋.

---
+++ WinterRain/ModConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinterRain
+{
+    class ModConfig
+    {
+        public bool useSnowDuration { get; set; }
+        public int[] snowDuration { get; set; }
+        public double chanceToRain { get; set; }
+        public double chanceToSnow { get; set; }
+
+
+
+        public ModConfig()
+        {
+            this.useSnowDuration = true;
+            this.snowDuration = new int[2] {8, 21};
+            this.chanceToRain = 0.183;
+            this.chanceToSnow = 0.63;
+        }
+    }
+}
